9522f1a0206e037f1a60bcc3a921fa98ffa5251b
amusing-bolt-9198 <edmund.nelson.b@gmail.com>
2019-01-08 04:17:03 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -11,3 +11,12 @@
 
 # However if you say 'I LOVE YOU GRANDMA!', she should respond with
 # 'I LOVE YOU TOO PUMPKIN!'
+def speak_to_grandma(str)
+  if str != str.upcase
+    return "HUH?! SPEAK UP, SONNY!"
+  elsif str =="I LOVE YOU GRANDMA!" 
+    return "I LOVE YOU TOO PUMPKIN!"
+  else
+    return "NO, NOT SINCE 1938!"
+  end
+end
